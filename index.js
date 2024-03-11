cf446c1b3fa314f633178c2ab4cb83bd7b0e0fbc
SugiSaku <155712184+SugiSaku8@users.noreply.github.com>
2024-03-11 23:26:21 +0000

    updata file

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const fs = require("fs")
 const base = {
+    //AutoRecoveryService
     arsJSON:function(pass){
-        let file = fs.readFileSync(pass);
-        try{
-        JSON.stringify(file)
+        fs.readFileSync(pass,'utf-8',function(){
+            try{
+                let orjson = JSON.parse(file);
+            }catch(err){
+                fs.appendFile(pass,'', function(err) {console.warn(`Error:${err}`);})
+            }
+        });     
+    },
+    ams:function(){
+const scanFolder = './';
+const quarantineFolder = '/chest';
+exec(`clamscan -r --move=${quarantineFolder} ${scanFolder}`, (error, stdout, stderr) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+});
     },
 }
